b724334f3603e74c7db57e6ab702f5a7c02126d0
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-08-10 10:37:08 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        TestandoThreads thread1 = new TestandoThreads("thread #1", 1000);
+
+        TestandoThreads thread2 = new TestandoThreads("thread #2", 500);
+
+    }
+}
---
+++ src/TestandoThreads.java
@@ -0,0 +1,26 @@
+
+public class TestandoThreads implements Runnable {
+
+    private String nome;
+    private Integer tempo;
+
+    public TestandoThreads(String nome, Integer tempo){
+        this.nome = nome;
+        this.tempo = tempo;
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run(){
+
+        for(int i = 0; i < 6; i++){
+            System.out.println(nome + " contador " + i);
+            try {
+                Thread.sleep(tempo);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
